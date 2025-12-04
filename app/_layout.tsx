095e533d9230e67f8e75b9941c56980a89a28168
ayoub akraou <87671584+ayoub-akraou@users.noreply.github.com>
2025-12-04 18:20:34 +0100

    feat(_layout): hide the default header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,22 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 import { StatusBar } from "expo-status-bar";
-import { Platform } from "react-native";
-import { Menu } from "lucide-react-native";
+import { Platform, View } from "react-native";
+import { Stack } from "expo-router";
+import Header from "../components/Header";
+
 const isIOS = Platform.OS === "ios";
 
 export default function RootLayout() {
 	return (
-		<SafeAreaView className={`flex-1 bg-emerald-400 ${isIOS ? "-mb-2" : "-mb-3"}`}>
-			<Menu />
-			<StatusBar />
-		</SafeAreaView>
+		<>
+			<StatusBar style="light" />
+			<SafeAreaView className={`flex-1 bg-black ${isIOS ? "-mb-2" : "-mb-3"}`}>
+				<Header />
+				<View className="bg-black text-white">
+					<Stack screenOptions={{ headerShown: false }} />
+				</View>
+			</SafeAreaView>
+		</>
 	);
 }
